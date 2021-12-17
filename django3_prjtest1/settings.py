3624c49de2237bfc14946d553bf463c760412030
dongzhuangdian <1032010805@qq.com>
2021-12-17 16:43:52 +0800

    debug

--- django3_prjtest1/settings.py
+++ django3_prjtest1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,10 +75,19 @@ WSGI_APPLICATION = 'django3_prjtest1.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'sitedata',    #需要手动创建数据库
+            'USER': 'root',
+            'PASSWORD': '123456',
+            'HOST': 'localhost',
+            'PORT': '3306',        #参见数据库my.ini文件内部配置
+    },
 }
 
 
